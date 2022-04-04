e38ddad42466e42dfcf7b82ecfcfcac8659f8d97
magnus.karlson <magnus.karlson@estcube.eu>
2022-04-04 03:24:36 +0300

    Implement RPC style message sending. Lamport timestamp counter

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ import random
 from threading import Thread
 
 if len(sys.argv) < 2 or int(sys.argv[1]) < 1:
-    raise Exception("Number of processes must be atleast 1!")
+    exit("Number of processes must be atleast 1!")
 
 total = int(sys.argv[1])
 max_p_timeout = 5
@@ -26,7 +26,9 @@ class Process(Thread):
     def __init__(self, name):
         Thread.__init__(self)
         self.name = name
-        self.timestamp = 0
+        self.timestamp = 0      # Lamport timestamp
+        self.queue = []
+        self.waitsResponse = []
         self.start_p_countdown()
 
     def start_p_countdown(self):
@@ -37,11 +39,62 @@ class Process(Thread):
         self.state = State.HELD
         self.countdown = random.randint(10, max_cs_timeout)
 
+    # Called by other process
+    def RPC_ask_access(self, p, timestamp):
+        # Process doesn't want to access cs
+        if self.state == State.DONOTWANT:
+            return 'ok'
+        
+        # Process itself wants to access critical section
+        elif self.state == State.WANTED:
+            # Req process has lower timestamp
+            if timestamp < self.timestamp:
+                return 'ok'
+            # Queue current process
+            else:
+                self.queue.append(p)
+                return None
+        # Process is accessing cs, queue req
+        elif self.state == State.HELD:
+            self.queue.append(p)
+            return None
+    
+    # Called by other processes to tell that, it has recieved one more approval
+    # RPC reciever for telling
+    def RPC_send_cs_finished(self, p):
+        self.waitsResponse.remove(p)
+        if len(self.waitsResponse) == 0:
+            self.start_cs_countdown()
+    
+    # Process asks other processes for access
+    # RPC sends access request to other processes
+    def send_access_req(self):
+        # Sends message to every other process
+        # If everyones response isn't ok, access isn't given
+        for p in processes:
+            if p == self: continue
+            res = p.RPC_ask_access(self, self.timestamp)
+            if res != 'ok':
+                self.waitsResponse.append(p)
+        # Give access to cs
+        if len(self.waitsResponse) == 0:
+            self.start_cs_countdown()
+
+    
+    # Sends message that are waiting for response, that it's finished accessing cs
+    # RPC sends message to other processes
+    def send_cs_finished(self):
+        for p in self.queue:
+            p.RPC_send_cs_finished(self)
+        self.queue = []
+
     def run(self):
         while True:
             if stopped: break
 
             time.sleep(1)
+            self.timestamp += 1
+
             if self.countdown == 0 or self.state not in [State.DONOTWANT, State.HELD]:
                 continue
             
@@ -51,13 +104,12 @@ class Process(Thread):
                 # Switch to wanted state
                 if self.state == State.DONOTWANT:
                     self.state = State.WANTED
-                    self.timestamp = time.time()
-                    cs_access()
+                    self.send_access_req()
 
                 # CS access has expired
                 elif self.state == State.HELD:
                     self.start_p_countdown()
-                    cs_access()
+                    self.send_cs_finished()
 
 
     def __str__(self):
@@ -68,19 +120,6 @@ for i in range(total):
     th = Process(f'P{i + 1}')
     th.start()
     processes.append(th)
-
-# Checks for processes that are waiting for access
-def cs_access():
-    res = None
-    for p in processes:
-        # Exits when some process has CS access
-        if p.state == State.HELD: return
-        # Checks if there exists process with WANTED state and it has the lower timestamp
-        if p.state == State.WANTED and (res == None or p.timestamp < res.timestamp):
-            res = p
-    # Grants cs access for process
-    if res != None:
-        res.start_cs_countdown()
 
 while True:
     msg = input("Enter command (list | info | time-cs | time-p | exit): ")
